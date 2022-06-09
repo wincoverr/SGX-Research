417340b87b5febf92bcdbda4a2cdccd5743fcc1a
Wincover <mathieu.pail2001@gmail.com>
2022-06-09 16:14:29 +0900

    let s start

---
+++ client/main.c
@@ -0,0 +1,25 @@
+//include
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+//include for socket
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+/*  
+    
+    Todo:       
+    1 - create a client that can connect to a client
+    2 - send message to server
+                          
+                            */
+                           
+int main(int argc, char *argv[])
+{
+    printf("Client Started");
+    printf("\n");
+
+}
---
+++ server/main.c
@@ -0,0 +1,27 @@
+//include
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+//include for socket
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+/*  
+    
+    Todo:       
+    1 - create a server that wait connexion of a client
+    2 - send data to client
+    3 - can receive data from client and print it on a txt file
+                          
+                            */
+
+int main(int argc, char *argv[])
+{
+    printf("Server Started");
+    printf("\n");
+
+}
